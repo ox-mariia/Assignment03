92937783789caff6a7d0bf59cec1a57bab2b9b32
ox-mariia <oxota.maria@gmail.com>
2020-02-26 22:21:58 -0800

    task3

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,17 +1,31 @@
 //STEP 1
-function halfNumber() {
-    let value = Number(window.prompt("Enter any number"));
-    window.document.write("Half of " + value + " is " + value/2 + ".");
-}
-halfNumber();
+// function halfNumber() {
+//     let value = Number(window.prompt("Enter any number"));
+//     window.document.write("Half of " + value + " is " + value/2 + ".");
+// }
+// halfNumber();
 
 //STEP 2
-function squarefNumber() {
-    let value = Number(window.prompt("Enter any number"));
-    window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + ".");
-}
- squarefNumber();
-//STEP 3
+// function squarefNumber() {
+//     let value = Number(window.prompt("Enter any number"));
+//     window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + ".");
+// }
+//  squarefNumber();
+
+//STEP 3 - v.1
+// function percentOf() {
+//     let num1 = Number(window.prompt("Enter the first number"));
+//     let num2 = Number(window.prompt("Enter the second number"));
+//     window.document.write(num1 + " is " + Math.round(num1/num2*100) + "% of " + num2 + ".");
+// }
+// percentOf();
+
+//STEP 3 - v.2
+// function percentOf(num1, num2)
+// {
+//   window.document.write(num1 + " is " + num1/num2*100 + "% of " + num2 + ".");
+// }
+// percentOf(2, 10);   
 
 //STEP 4
 
