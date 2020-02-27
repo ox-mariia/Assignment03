73bf8737248adcbfbf0f57138afab5044c0e26a2
ox-mariia <oxota.maria@gmail.com>
2020-02-26 21:27:44 -0800

    start task1

---
+++ js/01-functions-practice.js
@@ -0,0 +1,13 @@
+//STEP 1
+function halfNumber(number) {
+    let calc = Number(number)/2;
+    window.console.log("Half of" + number + "is " + cacl + ".");
+}
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
