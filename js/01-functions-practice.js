2007ef6d8f9f361140ed53e14fd7dd2a76a91065
ox-mariia <oxota.maria@gmail.com>
2020-02-26 22:00:56 -0800

    step2

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,11 +1,16 @@
 //STEP 1
-function halfNumber(number) {
-    let calc = Number(number)/2;
-    window.console.log("Half of" + number + "is " + cacl + ".");
+function halfNumber() {
+    let value = Number(window.prompt("Enter any number"));
+    window.document.write("Half of " + value + " is " + value/2 + ".");
 }
+halfNumber();
 
 //STEP 2
-
+function squarefNumber() {
+    let value = Number(window.prompt("Enter any number"));
+    window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + ".");
+}
+ squarefNumber();
 //STEP 3
 
 //STEP 4
