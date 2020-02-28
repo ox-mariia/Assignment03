2b293ed841a59df0cdb4601e0b34f29b52e2c5ce
ox-mariia <oxota.maria@gmail.com>
2020-02-27 18:09:43 -0800

    done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,16 +1,28 @@
-//STEP 1
+//STEP 1 - v.1
 // function halfNumber() {
 //     let value = Number(window.prompt("Enter any number"));
 //     window.document.write("Half of " + value + " is " + value/2 + ".");
 // }
 // halfNumber();
 
-//STEP 2
+//STEP 1 - v.2
+// function halfNumber(value) {
+//     window.document.write("Half of " + value + " is " + value/2 + ".");
+// }
+// halfNumber(7);
+
+//STEP 2 - v.1
 // function squarefNumber() {
 //     let value = Number(window.prompt("Enter any number"));
 //     window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + ".");
 // }
 //  squarefNumber();
+
+//STEP 2 - v.2
+// function squarefNumber(value) {
+//     window.document.write("The result of squaring the number " + value + " is " + Math.pow(value, 2) + ".");
+// }
+//  squarefNumber(7);
 
 //STEP 3 - v.1
 // function percentOf() {
@@ -27,6 +39,26 @@
 // }
 // percentOf(2, 10);   
 
-//STEP 4
+//STEP 4 - v.1
+// function findModulus() {
+//     let num1 = Number(window.prompt("Enter the first number"));
+//     let num2 = Number(window.prompt("Enter the second number"));
+//     window.document.write(num2 % num1 + " is the modulus of " + num1 + " and " + num2 + ".");
+// }
+// findModulus();
+
+// //STEP 4 - v.2
+// function findModulus(num1, num2) {
+//     window.document.write(num2 % num1 + " is the modulus of " + num1 + " and " + num2 + ".");
+//     }
+//     findModulus(4, 10);
 
 //STEP 5
+// function findSum(num1, num2, num3) {
+//     num1 = Number(window.prompt("Enter the first number"));
+//     num2 = Number(window.prompt("Enter the second number"));
+//     num3 = Number(window.prompt("Enter the third number"));
+//     let sum = num1 + num2 + num3;
+//     window.document.write("The sum of all of the arguments is " + sum + ".");
+// }
+// findSum();
