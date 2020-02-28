ea0f89b44874625a53503dcccc65e76935d75e65
ox-mariia <oxota.maria@gmail.com>
2020-02-27 18:28:05 -0800

    task2 done

---
+++ js/02-fortune-teller.js
@@ -0,0 +1,6 @@
+function tellFortune(childNum, partnerName, geoLocation, jobTitle) {
+    window.document.write("You will be a " + jobTitle + " in " + geoLocation + " and married to " + partnerName + " with "+ childNum + " kids.");
+}
+tellFortune(3, "Jason Momoa", "New Zealand", "Actress");
+tellFortune(2, "Andrew Sam", "California", "Spa owner");
+tellFortune(3, "Ryan Gosling", "Canada", "Dancer");
